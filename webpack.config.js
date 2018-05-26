1e0e8a1606d0e5f15666d8b51e9b1011cdb77e7b
Michael Ruan <michaelruan1@gmail.com>
2018-05-26 17:00:31 -0400

    got webpack running

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/discomms.jsx',
+  output: {
+    path: path.resolve(__dirname, "./app/assets/javascripts"),
+    filename: 'bundle.js'
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
